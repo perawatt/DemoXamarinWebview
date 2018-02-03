cbac1105d8720a05bb32a77300ef720786593743
Perawatt <pekung4433@perfenterprise.com>
2018-02-03 17:35:15 +0700

    initial Project

---
+++ DemoXamarinWebview.UITest/Tests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace DemoXamarinWebview.UITest
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            //app.Repl();
+
+            //Arraning
+            string inputId = "#lst-ib";
+            string inputText = "xamarin";
+            string buttonId = "#tsbb";
+            Func<AppQuery, AppQuery> webView = e => e.Marked("myWebview");
+
+            //Action
+            app.WaitForElement(webView, "Time out");
+            app.Tap(c => c.WebView(0).Css(inputId));
+            app.EnterText(inputText);
+            app.Tap(c => c.WebView(0).Css(buttonId));
+            app.WaitForElement(c => c.WebView(0).Css(inputId), "Time out");
+
+            //Asserting
+            var assert = app.Query(w => w.WebView().InvokeJS("return document.getElementById('lst-ib').value"))[0];
+            Assert.AreEqual(inputText, assert);
+        }
+    }
+}
+
---
+++ DemoXamarinWebview/DemoXamarinWebview/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoXamarinWebview
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            webView.Source = "https://www.google.co.th/";
+        }
+        void backButtonClicked(object sender, EventArgs e)
+        {
+            if (webView.CanGoBack)
+            {
+                webView.GoBack();
+            }
+        }
+
+        /// <summary>
+        /// Navigates forward
+        /// </summary>
+        void forwardButtonClicked(object sender, EventArgs e)
+        {
+            if (webView.CanGoForward)
+            {
+                webView.GoForward();
+            }
+        }
+    }
+}
